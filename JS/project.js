0397d5f16c6c1dce606d8bb3487407d3360d6950
abdelrahman212007 <137640784+abdelrahman212007@users.noreply.github.com>
2023-08-03 13:49:19 +0300

    Update project.

--- JS/project.js
+++ JS/project.js
@@ -1,11 +1,1 @@
 
-let btn = document.querySelector("button");
-let nav = document.querySelector("nav");
-
-
-btn.addEventListener("click", function() {
-
-    nav.classList.toggle("active")
-    btn.classList.toggle("active")
-
-});
